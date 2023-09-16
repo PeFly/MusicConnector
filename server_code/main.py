93c3fcad84609a1b9f565b4b10383ff08ef57350
future.and.fly@gmail.com <future.and.fly@gmail.com>
2023-09-16 17:49:54 +0000

    Edited main and result

--- server_code/main.py
+++ server_code/main.py
@@ -3,17 +3,17 @@
 ###########################
 
 
+##### MODULES #####
 import anvil.tables as tables
 import anvil.tables.query as q
 from anvil.tables import app_tables
+import anvil.server
 import anvil.users
 import anvil.http
 
-
-
-##### MODULES #####
-import anvil.server
 import re
+
+
 
 
 ##### CLIENT CALLABLE #####
